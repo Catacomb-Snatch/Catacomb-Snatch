b12ef83a6512c6a423bbafe84792e68231e9df1f
SpaceEmotion <spaceemotion@gmail.com>
2013-01-10 13:25:25 +0100

    Add screen test to main class

--- core/src/main/java/net/catacombsnatch/game/core/CatacombSnatchGame.java
+++ core/src/main/java/net/catacombsnatch/game/core/CatacombSnatchGame.java
@@ -1,23 +1,36 @@
 package net.catacombsnatch.game.core;
 
+import net.catacombsnatch.game.core.screen.Art;
+import net.catacombsnatch.game.core.screen.Screen;
+
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 
 public class CatacombSnatchGame implements ApplicationListener {
-	public void create () {
+	private Screen screen;
+
+	public void create() {
+		if ( !Art.loadResources() ) Gdx.app.exit();
+
+		screen = new Screen();
 	}
 
-	public void resize (int width, int height) {
+	public void resize( int width, int height ) {
 	}
 
-	public void render () {
+	public void render() {
+		screen.getGraphics().begin();
+		screen.clear( Color.BLACK );
+		screen.getGraphics().end();
 	}
 
-	public void pause () {
+	public void pause() {
 	}
 
-	public void resume () {
+	public void resume() {
 	}
 
-	public void dispose () {
+	public void dispose() {
 	}
 }
