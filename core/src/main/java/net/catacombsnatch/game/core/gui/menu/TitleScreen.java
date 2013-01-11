f149b2364f66d90ee67478c80c7e491f9c6ccc2d
SpaceEmotion <spaceemotion@gmail.com>
2013-01-11 13:28:25 +0100

    Just a small library test for the title screen

--- core/src/main/java/net/catacombsnatch/game/core/gui/menu/TitleScreen.java
+++ core/src/main/java/net/catacombsnatch/game/core/gui/menu/TitleScreen.java
@@ -3,15 +3,29 @@ package net.catacombsnatch.game.core.gui.menu;
 import net.catacombsnatch.game.core.screen.Art;
 import net.catacombsnatch.game.core.screen.Screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.HAlignment;
 
 public class TitleScreen extends GuiMenu {
+	private BitmapFont font;
 	private int selected = 0;
+
+	public TitleScreen() {
+		font = new BitmapFont();
+	}
+
+	public void exit() {
+		font.dispose();
+	}
 
 	public void render( Screen screen ) {
 		screen.getGraphics().draw( Art.titleScreen, 0, 0 );
+		screen.fill( Screen.getWidth() / 2 - 50, Screen.getHeight() / 2 - selected * 30, 100, 20, Color.BLACK );
 
-		screen.fill( 10, 10 + selected * 30, 100, 20, Color.RED );
+		font.drawMultiLine( screen.getGraphics(), "Exit game", Screen.getWidth() / 2, 50, 0, HAlignment.CENTER );
+		font.draw( screen.getGraphics(), Integer.toString( Gdx.graphics.getFramesPerSecond() ) + " FPS", 2, Screen.getHeight() - 2 );
 	}
 
 	public boolean scrolled( int amount ) {
