196cc9a260751b3b55cdba6121fb441b9119fb77
SpaceEmotion <spaceemotion@gmail.com>
2013-01-14 12:18:04 +0100

    Remove abstract identifier from ISoundPlayer methods and add default sound tracks

--- core/src/main/java/net/catacombsnatch/game/core/sound/ISoundPlayer.java
+++ core/src/main/java/net/catacombsnatch/game/core/sound/ISoundPlayer.java
@@ -2,31 +2,53 @@ package net.catacombsnatch.game.core.sound;
 
 public interface ISoundPlayer {
 
-	public static final String BACKGROUND_TRACK = "background";
+	/** The title theme track name */
+	public static final String TITLE_THEME = "ThemeTitle";
+
+	/** The end theme track name */
+	public static final String END_THEME = "ThemeEnd";
+
+	/** First background theme track name */
+	public static final String BACKGROUND_TRACK_1 = "Background 1";
+
+	/** Second background theme track name */
+	public static final String BACKGROUND_TRACK_2 = "Background 2";
+
+	/** Third background theme track name */
+	public static final String BACKGROUND_TRACK_3 = "Background 3";
+
+	/** Fourth background theme track name */
+	public static final String BACKGROUND_TRACK_4 = "Background 4";
+
+	/** The "Shot 1" sound name */
+	public static final String SOUND_SHOT_1 = "Shot 1";
+
+	/** The Explosion sound name */
+	public static final String SOUND_EXPLOSION = "Explosion";
 
 	/** Starts the title music */
-	public abstract void startTitleMusic();
+	public void startTitleMusic();
 
 	/** Stops the title music */
-	public abstract void stopTitleMusic();
+	public void stopTitleMusic();
 
 	/** Starts the end music */
-	public abstract void startEndMusic();
+	public void startEndMusic();
 
 	/** Stops the end music */
-	public abstract void stopEndMusic();
+	public void stopEndMusic();
 
 	/** Starts the background playlist */
-	public abstract void startBackgroundMusic();
+	public void startBackgroundMusic();
 
 	/** Stops the background playlist */
-	public abstract void stopBackgroundMusic();
+	public void stopBackgroundMusic();
 
 	/** Pauses the background playlist */
-	public abstract void pauseBackgroundMusic();
+	public void pauseBackgroundMusic();
 
 	/** Resumes the background playlist */
-	public abstract void resumeBackgroundMusic();
+	public void resumeBackgroundMusic();
 
 	/**
 	 * Sets the listener position
@@ -34,7 +56,7 @@ public interface ISoundPlayer {
 	 * @param x The x-position
 	 * @param y The y-position
 	 */
-	public abstract void setListenerPosition( float x, float y );
+	public void setListenerPosition( float x, float y );
 
 	/**
 	 * Plays a sound with a given name at a specific location
@@ -44,7 +66,7 @@ public interface ISoundPlayer {
 	 * @param y The y-position
 	 * @return True if the sound could be played, otherwise false
 	 */
-	public abstract boolean playSound( String sourceName, float x, float y );
+	public boolean playSound( String sourceName, float x, float y );
 
 	/**
 	 * Plays a sound with a given name at a specific location with sound
@@ -56,23 +78,23 @@ public interface ISoundPlayer {
 	 * @param blocking True if the sound should be blocked, otherwise false
 	 * @return True if the sound could be played, otherwise false
 	 */
-	public abstract boolean playSound( String sourceName, float x, float y, boolean blocking );
+	public boolean playSound( String sourceName, float x, float y, boolean blocking );
 
 	/** Shuts down the sound system */
-	public abstract void shutdown();
+	public void shutdown();
 
 	/**
 	 * Returns true if the system is muted
 	 * 
 	 * @return True if the system is muted, otherwise false
 	 */
-	public abstract boolean isMuted();
+	public boolean isMuted();
 
 	/**
 	 * Mutes the sound system
 	 * 
 	 * @param muted True for muting, false for unmuting
 	 */
-	public abstract void setMuted( boolean muted );
+	public void setMuted( boolean muted );
 
 }
