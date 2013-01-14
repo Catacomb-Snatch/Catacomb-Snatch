6cece6f0d56f20d70bc06a80a10f49f71f9aba91
SpaceEmotion <spaceemotion@gmail.com>
2013-01-14 15:40:09 +0100

    Add TestGameMenu

---
+++ core/src/main/java/net/catacombsnatch/game/core/gui/menu/TestGameMenu.java
@@ -0,0 +1,25 @@
+package net.catacombsnatch.game.core.gui.menu;
+
+import net.catacombsnatch.game.core.Game;
+import net.catacombsnatch.game.core.screen.Screen;
+
+import com.badlogic.gdx.Input.Keys;
+
+public class TestGameMenu extends GuiMenu {
+
+	@Override
+	public void render( Screen screen ) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean keyDown( int k ) {
+		if ( k == Keys.ESCAPE ) {
+			Game.exitMenu();
+			return true;
+		}
+
+		return false;
+	}
+
+}
--- core/src/main/java/net/catacombsnatch/game/core/gui/menu/TitleScreen.java
+++ core/src/main/java/net/catacombsnatch/game/core/gui/menu/TitleScreen.java
@@ -1,9 +1,13 @@
 package net.catacombsnatch.game.core.gui.menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.catacombsnatch.game.core.Game;
 import net.catacombsnatch.game.core.entity.Entity;
 import net.catacombsnatch.game.core.entity.EntityManager;
 import net.catacombsnatch.game.core.entity.components.Animated;
+import net.catacombsnatch.game.core.input.Input;
 import net.catacombsnatch.game.core.resources.Fonts;
 import net.catacombsnatch.game.core.screen.Art;
 import net.catacombsnatch.game.core.screen.Screen;
@@ -14,11 +18,19 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont.HAlignment;
 
 public class TitleScreen extends GuiMenu {
 	private int selected = 0;
+	private List<String> menu;
+
 	private Entity charEntity;
 
 	public TitleScreen() {
+		menu = new ArrayList<String>();
+		menu.add( "START DEMO" );
+		menu.add( "START GAME" );
+		menu.add( "OPTIONS" );
+		menu.add( "EXIT GAME" );
+
 		charEntity = new EntityManager().createEntity();
-		Animated comp = charEntity.addComponent( Animated.class, new Animated( Art.lordLard[0], 0.15f ) );
+		charEntity.addComponent( Animated.class, new Animated( Art.lordLard[0], 0.15f ) );
 		scrolled( 0 );
 
 		Game.soundPlayer.startTitleMusic();
@@ -31,9 +43,8 @@ public class TitleScreen extends GuiMenu {
 	public void render( Screen screen ) {
 		screen.getGraphics().draw( Art.titleScreen, 0, 0 );
 
-		Fonts.GOLD.drawMultiLine( screen.getGraphics(), "START GAME", Screen.getWidth() / 2, 82, 0, HAlignment.CENTER );
-		Fonts.GOLD.drawMultiLine( screen.getGraphics(), "OPTIONS", Screen.getWidth() / 2, 66, 0, HAlignment.CENTER );
-		Fonts.GOLD.drawMultiLine( screen.getGraphics(), "EXIT GAME", Screen.getWidth() / 2, 50, 0, HAlignment.CENTER );
+		for ( int s = 0; s < menu.size(); s++ )
+			Fonts.GOLD.drawMultiLine( screen.getGraphics(), menu.get( s ), Screen.getWidth() / 2, 128 - 24 * s, 0, HAlignment.CENTER );
 
 		Fonts.GOLD.draw( screen.getGraphics(), Integer.toString( Gdx.graphics.getFramesPerSecond() ) + " FPS", 2, 2 );
 
@@ -43,26 +54,33 @@ public class TitleScreen extends GuiMenu {
 	public boolean scrolled( int amount ) {
 		selected -= amount;
 
-		if ( selected > 2 )
+		if ( selected > menu.size() - 1 )
 			selected = 0;
-		else if ( selected < 0 ) selected = 2;
+		else if ( selected < 0 ) selected = menu.size() - 1;
 
-		charEntity.getComponent( Animated.class ).setPosition( (Screen.getWidth() - 200) / 2, 82 - 8 + (-selected * 16) );
+		charEntity.getComponent( Animated.class ).setPosition( (Screen.getWidth() - 200) / 2, 128 - 8 + (-selected * 24) );
 
 		return true;
 	}
 
-	public boolean keyUp( int key ) {
-		switch ( key ) {
-		case Keys.ENTER:
+	public boolean keyDown( int key ) {
+		if ( key == Keys.ENTER ) {
 			switch ( selected ) {
 			case 0:
-			case 1:
+				Game.switchTo( TestGameMenu.class );
 				break;
 
+			case 1:
 			case 2:
+				break;
+
+			case 3:
 				Game.exitAll();
 			}
+		} else if ( key == Input.up ) {
+			scrolled( 1 );
+		} else if ( key == Input.down ) {
+			scrolled( -1 );
 		}
 
 		return true;
