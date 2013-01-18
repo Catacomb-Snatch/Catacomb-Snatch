28f5959515c4f79b6e658bde3fd4d84820372e1e
SpaceEmotion <spaceemotion@gmail.com>
2013-01-18 07:45:19 +0100

    Create world in testgamemenu

--- core/src/main/java/net/catacombsnatch/game/core/gui/menu/TestGameMenu.java
+++ core/src/main/java/net/catacombsnatch/game/core/gui/menu/TestGameMenu.java
@@ -2,14 +2,19 @@ package net.catacombsnatch.game.core.gui.menu;
 
 import net.catacombsnatch.game.core.Game;
 import net.catacombsnatch.game.core.screen.Screen;
+import net.catacombsnatch.game.core.world.Difficulty;
+import net.catacombsnatch.game.core.world.World;
 
 import com.badlogic.gdx.Input.Keys;
 
 public class TestGameMenu extends GuiMenu {
+	protected World world;
+
+	public TestGameMenu() {
+		this.world = new World( Difficulty.EASY );
+	}
 
 	public void render( Screen screen ) {
-		// TODO Auto-generated method stub
-
 	}
 
 	public boolean keyDown( int k ) {
