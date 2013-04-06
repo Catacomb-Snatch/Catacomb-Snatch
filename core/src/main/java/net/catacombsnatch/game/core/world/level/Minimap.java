dc6c416271e6bfdad594171990ee15a168a3e5fc
AngelDE98 <nuda1998@gmail.com>
2013-04-06 17:48:34 +0200

    Added testing minimap [ci skip]

--- core/src/main/java/net/catacombsnatch/game/core/world/level/Minimap.java
+++ core/src/main/java/net/catacombsnatch/game/core/world/level/Minimap.java
@@ -4,22 +4,46 @@ import net.catacombsnatch.game.core.resources.Art;
 import net.catacombsnatch.game.core.scene.Scene;
 import net.catacombsnatch.game.core.screen.Renderable;
 import net.catacombsnatch.game.core.screen.Screen;
+import net.catacombsnatch.game.core.world.tile.Tile;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Minimap implements Renderable {
 	protected Level level;
 	protected Sprite sprite;
 	
+	protected Texture map;
+	protected Pixmap pm;
+	
 	public Minimap(Level level) {
 		this.level = level;
 		
 		sprite = new Sprite(Art.skin.getAtlas().findRegion("minimap-frame"));
+		
+		pm = new Pixmap( 40, 40, Pixmap.Format.RGBA8888 );
+		map = new Texture( pm, true);
+		pm = new Pixmap( 40, 40, Pixmap.Format.RGBA8888 );
 	}
 
 	@Override
 	public void render(Scene scene) {
 		sprite.draw(scene.getSpriteBatch());
+		
+		for (Layer layer : level.layers) {
+			for (Tile tile : layer.tiles) {
+				Color c = tile.getMinimapColor();
+				int x = (int) tile.getBounds().x;
+				int y = (int) tile.getBounds().y;
+				pm.setColor(c);
+				pm.drawRectangle(x, y, 1, 1);
+			}
+		}
+		map.draw(pm, 0, 0);
+		
+		scene.getSpriteBatch().draw(map, sprite.getX() + 6, sprite.getY() + 5, 80, 80);
 	}
 	
 	public void update(boolean resize) {
