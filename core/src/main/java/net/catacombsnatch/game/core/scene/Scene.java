4ff817f2a1becf4a86d1eeb0323adb139a4998bb
AngelDE98 <nuda1998@gmail.com>
2013-04-07 17:23:54 +0200

    Fixed hover for text buttons [ci skip]

--- core/src/main/java/net/catacombsnatch/game/core/scene/Scene.java
+++ core/src/main/java/net/catacombsnatch/game/core/scene/Scene.java
@@ -5,8 +5,12 @@ import net.catacombsnatch.game.core.screen.Screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -47,12 +51,38 @@ public class Scene extends Stage {
 	 */
 	public void update(boolean resize) {}
 	
+	Rectangle r = new Rectangle();
+	Vector2 v2 = new Vector2();
+	
 	public void render() {
 		// Draw background
 		if(background != null) {
 			getSpriteBatch().begin();
 			getSpriteBatch().draw(background, 0, 0, Screen.getWidth(), Screen.getHeight());
 			getSpriteBatch().end();
+		}
+		
+		//Similar to Google's Android "Project Butter":
+		//Updating mouse dependent stuff in render() 
+		if (!Gdx.input.isTouched()) {
+			int mx = Gdx.input.getX();
+			int my = Gdx.input.getY();
+			v2.set(mx, my);
+			v2 = screenToStageCoordinates(v2);
+			//System.out.println("new: "+v2);
+			for (Actor a : getActors()) {
+				r.set(a.getX(), a.getY(), a.getWidth(), a.getHeight());
+				for (EventListener el : a.getListeners()) {
+					if (el instanceof InputListener) {
+						InputListener il = (InputListener) el;
+						if (r.contains(v2.x, v2.y)) {
+							il.enter(null, v2.x, v2.y, -1, null);
+						} else {
+							il.exit(null, v2.x, v2.y, -1, null);
+						}
+					}
+				}
+			}
 		}
 		
 		super.draw();
