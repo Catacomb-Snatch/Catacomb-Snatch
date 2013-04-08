4d9cfd2f7d0824af0d086ad198696e80eb5c216d
SpaceEmotion <spaceemotion@gmail.com>
2013-04-08 20:03:42 +0200

    Add basic achievement logic! [ci skip]

---
+++ core/src/main/java/net/catacombsnatch/game/core/achievement/AchievementType.java
@@ -0,0 +1,19 @@
+package net.catacombsnatch.game.core.achievement;
+
+import net.catacombsnatch.game.core.resources.Language;
+
+public enum AchievementType {
+	DEFAULT("achievement.type.default"),
+	HIDDEN("achievement.type.hidden");
+	
+	private final String name;
+	
+	private AchievementType(String name) {
+		this.name = name;
+	}
+	
+	/** @return The language-specific formatted name for this type */
+	public String getName() {
+		return Language.get(name);
+	}
+}
