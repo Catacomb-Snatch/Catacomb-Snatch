e70a962e369390aeae9c42a6ec65708af5275812
64BitUniverse <64bituniverse@gmail.com>
2020-01-28 12:38:55 -0600

    New Scripts/Rotate Loard Lard 90 x axis
    New scripts so that the player actually cane move now, and we need to rotate all prefabs 90 degress on the X axis

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 playerVelocity)
+    {
+        velocity = playerVelocity;
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
