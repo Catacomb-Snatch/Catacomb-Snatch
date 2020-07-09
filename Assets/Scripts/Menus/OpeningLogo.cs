9536b90689d232c49c73d5e7730a2538c00dcaa6
64BitUniverse <64bituniverse@gmail.com>
2020-07-09 13:36:56 -0400

    Lots of stuff
    I did a ton of stuff, so I'm just going to upload it all. I also updated the project to 2019.4, so there's that. I've made UI elements in the Main Menu Scene, and am working on getting idle anims for all characters, making a minimap, and giving the Enemys brains

---
+++ Assets/Scripts/Menus/OpeningLogo.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class OpeningLogo : MonoBehaviour
+{
+    //Raw Image to Show Video Images [Assign from the Editor]
+    public RawImage image;
+    //Video To Play [Assign from the Editor]
+    public VideoClip videoToPlay;
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+
+    //Audio
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+    }
+
+    IEnumerator playVideo()
+    {
+        //Add VideoPlayer to the GameObject
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        //Add AudioSource
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Disable Play on Awake for both Video and Audio
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+
+        //We want to play from video clip not from url
+        videoPlayer.source = VideoSource.VideoClip;
+
+        //Set Audio Output to AudioSource
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        //Assign the Audio from Video to AudioSource to be played
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        //Set video To Play then prepare Audio to prevent Buffering
+        videoPlayer.clip = videoToPlay;
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        while (!videoPlayer.isPrepared)
+        {
+            Debug.Log("Preparing Video");
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+
+        //Assign the Texture from Video to RawImage to be displayed
+        image.texture = videoPlayer.texture;
+
+        //Play Video
+        videoPlayer.Play();
+
+        //Play Sound
+        audioSource.Play();
+
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+
+        Debug.Log("Done Playing Video");
+    }
+}
---
+++ Assets/Scripts/Menus/PlayerSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    public void Selected()
+    {
+        if (anim != null)
+        {
+            anim.Play("Lard_Right_Gun");
+        }
+    }
+}
