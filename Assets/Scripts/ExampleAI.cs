c6bc7c899f5a89d93399c04c3bfe6a7e7fa53ec8
64BitUniverse <64bituniverse@gmail.com>
2020-07-12 23:05:32 -0400

    0.0.0.1/2
    Super Duper Alpha Time (SDAT)

---
+++ Assets/Scripts/ExampleAI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ExampleAI : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(path == null)
+        {
+            return;
+        }
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        } else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
--- Assets/Scripts/Player-Turrets-Enemy/Enemy.cs
+++ Assets/Scripts/Player-Turrets-Enemy/Enemy.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
+
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 movement;
+    public Animator animator;
+
+    public AIPath aiPath;
+
     void Start()
     {
+
+
+
         
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        print("Pos X:" + aiPath.desiredVelocity.y);
+        print("Pos Y:" + aiPath.desiredVelocity.x);
+
+        if (aiPath.desiredVelocity.y >= 0.01f)
+        {
+            animator.SetFloat("Horizontal", 1);
+        } else if (aiPath.desiredVelocity.y <= -0.01)
+        {
+            animator.SetFloat("Horizontal", -1);
+        }
+
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            animator.SetFloat("Vertical", 1);
+        } else if (aiPath.desiredVelocity.x <= -0.01)
+        {
+            animator.SetFloat("Vertical", -1);
+        }
+
     }
 }
